abae3f80a265d3744b3393479221e275263b11eb
Sigadong <irwanto.regar30@gmail.com>
2022-09-23 01:05:24 +0700

    practice: using path parameters in SPA with react router (Practice: Using Path Parameters in SPA with React Router.)

--- src/index.js
+++ src/index.js
@@ -1,47 +1,9 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-
-function HomePage() {
-  return <p>This is Homepage</p>;
-}
-
-function AboutPage() {
-  return <p>This is About page</p>;
-}
-
-function FAQPage() {
-  return <p>This is FAQ page</p>;
-}
-
-function App() {
-  return (
-    <>
-      <header>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/faq">Faq</Link>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/faq" element={<FAQPage />} />
-        </Routes>
-      </main>
-    </>
-  );
-}
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
     <App />
