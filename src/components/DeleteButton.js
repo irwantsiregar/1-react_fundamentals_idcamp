f0fc0c9e4071a075d17bb79b036617bfc8855b98
Sigadong <irwanto.regar30@gmail.com>
2022-09-23 13:42:24 +0700

    case study exercise: creating a contact search feature

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { FiDelete } from 'react-icons/fi';
 
 function DeleteButton({ id, onDelete }) {
-  return <button className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+  return <button className='contact-item__delete' onClick={() => onDelete(id)}><FiDelete /></button>
 }
 
 export default DeleteButton;
+
+DeleteButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}
+ 
