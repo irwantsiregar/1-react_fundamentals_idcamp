efee09b3277d8617fe031d25dd36a7eb4c02cd20
Sigadong <irwanto.regar30@gmail.com>
2022-10-03 08:19:48 +0700

    display a list of repositories with a choice of programming languages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import NotesApp from './components/NotesApp';
-import './assets/styles/index.css';
-import './assets/js/script';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import App from './App';
 
-AOS.init({ once: true, duration: 1500 });
-library.add(fas);
+import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <NotesApp />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
+root.render(<App />);
