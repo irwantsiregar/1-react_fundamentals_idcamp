98d09c07fba9d86e2f158020397eb9738791a24d
Sigadong <irwanto.regar30@gmail.com>
2022-10-07 00:44:01 +0700

    react context dengan useContext()

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AnimalFactCard from './components/AnimalFactCard';
+const LocaleContext = React.createContext('id');
+const ThemeContext = React.createContext('light');
+const UserContext = React.createContext({ id: 'user-1', name: 'Dicoding' });
 
-import './styles/style.css';
-
-function App() {
-  const [animal, setAnimal] = React.useState('cat');
-
-  const genderChangeHandler = (event) => setAnimal(event.target.value);
+function Content() {
+  const locale = React.useContext(LocaleContext);
+  const theme = React.useContext(ThemeContext);
+  const user = React.useContext(UserContext);
 
   return (
-    <>
-      <select onChange={genderChangeHandler}>
-        <option value="cat">Cat Fact</option>
-        <option value="dog">Dog Fact</option>
-      </select>
-      <AnimalFactCard animal={animal} />
-    </>
+    <Article locale={locale} theme={theme} user={user} />
   );
 }
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
